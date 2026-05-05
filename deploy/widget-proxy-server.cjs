59cfe54edfdbf3c1fca6eda67008ae9cd49f48a6
patabrava <caposk8@hotmail.com>
2026-05-05 13:51:16 +0200

    fix: stabilize demo widget proxy startup

---
+++ deploy/widget-proxy-server.cjs
@@ -0,0 +1,19 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const distDir = '/app/repo/widget/dist';
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'http://sarah-backend:3000',
+    changeOrigin: false,
+    ws: true,
+  }),
+);
+
+app.use(express.static(distDir));
+app.use((req, res) => res.sendFile(`${distDir}/index.html`));
+
+app.listen(8080, '0.0.0.0');
